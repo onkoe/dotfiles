15bb623f9fcee6b1f000862d87bb7c97fb3d22f2
Barrett Ray <onkoe@protonmail.com>
2023-04-23 19:34:29 -0500

    Added my little Wayland fix for VSCode to dotfiles

---
+++ 22/.config/fish/functions/code.fish
@@ -0,0 +1,3 @@
+function code --description 'alias code code -enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland'
+ command code -enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland $argv; 
+end
