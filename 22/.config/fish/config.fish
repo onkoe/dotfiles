5b3b0643c72dd450c82f364094dee29a00cf3e7a
Barrett Ray <onkoe@protonmail.com>
2023-05-14 15:41:03 -0500

    Added config.fish for some basic shell setup

---
+++ 22/.config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# enable grc (colorizer)
+# https://github.com/garabik/grc
+source /etc/grc.fish
+
+
+# shorthand for gnome text editor
+alias gte gnome-text-editor
+
